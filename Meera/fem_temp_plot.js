ff4734b3341683f339c123878afcf21b65df727c
msharma-26 <mksharma@ucsd.edu>
2025-05-06 17:40:36 -0700

    added second graph to home.html

--- Meera/fem_temp_plot.js
+++ Meera/fem_temp_plot.js
@@ -1,6 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-const svg = d3.select("svg");
+const svg = d3.select("#female_temp_line");
 const margin = { top: 20, right: 100, bottom: 50, left: 50 };
 const width = +svg.attr("width") - margin.left - margin.right;
 const height = +svg.attr("height") - margin.top - margin.bottom;
@@ -77,4 +77,97 @@ svg.append("text")
       .attr("stroke-width", 1.5)
       .attr("d", line);
   });
+});
+
+
+const svg2 = d3.select("#comp_plot");
+const margin2 = { top: 20, right: 100, bottom: 50, left: 50 };
+const width2 = +svg2.attr("width") - margin2.left - margin2.right;
+const height2 = +svg2.attr("height") - margin2.top - margin2.bottom;
+const g2 = svg2.append("g").attr("transform", `translate(${margin2.left},${margin2.top})`);
+
+Promise.all([
+  d3.csv("./data/fem_mouse_act.csv"),
+  d3.csv("./data/male_mouse_act.csv")
+]).then(([femData, maleData]) => {
+  const time = d3.range(femData.length);
+
+  function computeAverages(data) {
+    const ids = data.columns;
+    return time.map((t, i) => {
+      const sum = d3.sum(ids, id => +data[i][id]);
+      return { time: t, avg: sum / ids.length };
+    });
+  }
+
+  const femAvg = computeAverages(femData);
+  const maleAvg = computeAverages(maleData);
+
+  const x = d3.scaleLinear().domain([0, 20160]).range([0, width2]);
+  const y = d3.scaleLinear()
+    .domain([0, d3.max([...femAvg, ...maleAvg], d => d.avg)])
+    .nice()
+    .range([height2, 0]);
+
+  // Axes
+  g2.append("g")
+    .attr("transform", `translate(0,${height2})`)
+    .call(d3.axisBottom(x).ticks(14).tickFormat(d => `Day ${Math.floor(d / 1440)}`));
+  g2.append("g")
+    .call(d3.axisLeft(y).tickFormat(d => d.toFixed(1)));
+
+  // X Axis Label
+  svg2.append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", margin2.left + width2 / 2)
+    .attr("y", margin2.top + height2 + 40)
+    .text("Time (Days)");
+
+  // Y Axis Label
+  svg2.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", `rotate(-90)`)
+    .attr("x", -(margin2.top + height2 / 2))
+    .attr("y", 15)
+    .text("Activity Level (AU)");
+
+  // Estrus shading
+  for (let t = estrusStart; t < 20160; t += estrusPeriod) {
+    g2.append("rect")
+      .attr("x", x(t))
+      .attr("width", x(t + estrusDuration) - x(t))
+      .attr("y", 0)
+      .attr("height", height2)
+      .attr("fill", "pink")
+      .attr("opacity", 0.3);
+  }
+
+  // Line generator
+  const line = d3.line()
+    .x(d => x(d.time))
+    .y(d => y(d.avg));
+
+  g2.append("path")
+    .datum(femAvg)
+    .attr("fill", "none")
+    .attr("stroke", "deeppink")
+    .attr("stroke-width", 2)
+    .attr("d", line);
+
+  g2.append("path")
+    .datum(maleAvg)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 2)
+    .attr("d", line);
+
+  // Legend
+  const legend = svg2.append("g")
+    .attr("transform", `translate(${margin2.left + width2 + 10}, ${margin2.top})`);
+
+  legend.append("circle").attr("r", 5).attr("fill", "deeppink").attr("cy", 0);
+  legend.append("text").text("Female Avg").attr("x", 10).attr("y", 5);
+
+  legend.append("circle").attr("r", 5).attr("fill", "steelblue").attr("cy", 20);
+  legend.append("text").text("Male Avg").attr("x", 10).attr("y", 25);
 });
